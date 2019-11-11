9a2af0f064da4b41a22bd9585f6e4e7f9531f4cc
johanblumenberg <johan.blumenberg@symphony.com>
2019-11-11 23:15:43 +0100

    Add methods to interface

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,3 +1,10 @@
 export interface Logger {
     logAccount(id: string);
+
+    logUser(id: string): void;
+    logChair(): void;
+    logHouse(): void;
+    logExtra(): void;
+    logOver(): void;
+    logUnder(): void;
 }
--- test/jest/__mocks__/Logger.mock.ts
+++ test/jest/__mocks__/Logger.mock.ts
@@ -2,4 +2,11 @@ import { Logger } from "../../../src/Logger";
 
 export class LoggerMock implements Logger {
     logAccount = jest.fn<void, [string]>();
+
+    logUser = jest.fn<void, [string]>();
+    logChair = jest.fn<void, []>();
+    logHouse = jest.fn<void, []>();
+    logExtra = jest.fn<void, []>();
+    logOver = jest.fn<void, []>();
+    logUnder = jest.fn<void, []>();
 }
